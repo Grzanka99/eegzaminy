e5fbe1ab20d9387ea4dbefdbda2659f9082ad288
Cezary Grzanka <cezarygrzanka@gmail.com>
2019-05-02 14:59:23 +0200

    Move account endpoints to access.py

--- api/app/resources/v1/__init__.py
+++ api/app/resources/v1/__init__.py
@@ -6,3 +6,4 @@ api_v1 = Blueprint('v1', __name__)
 from .exams import *
 from .greeting import *
 from .admin import *
+from .access import *
---
+++ api/app/resources/v1/access.py
@@ -0,0 +1,101 @@
+from ..base import BaseResource
+from ...models import UserModel, RevokedTokenModel
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    jwt_refresh_token_required,
+    get_jwt_identity,
+    get_raw_jwt
+)
+from flask_restful import reqparse
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('username', help='This field cannot be blank', required=True)
+parser.add_argument('password', help='This field cannot be blank', required=True)
+
+
+class UserRegistration(BaseResource):
+    def post(self):
+        data = parser.parse_args()
+
+        if UserModel.find_by_username(data['username']):
+            self.response(
+                {'message': 'User {} already exists'.format(data['username'])}, 409
+            )
+
+        new_user = UserModel(
+            username=data['username'],
+            password=UserModel.generate_hash(data['password'])
+        )
+
+        try:
+            new_user.add_user()
+            access_token = create_access_token(identity=data['username'])
+            refresh_token = create_refresh_token(identity=data['username'])
+            self.response(
+                {
+                    'message': 'User {} was created'.format(data['username']),
+                    'accessToken': access_token,
+                    'refreshToken': refresh_token
+                }, 201
+            )
+        except:
+            self.response({'message': 'Something went wrong'}, 500)
+
+
+class UserLogin(BaseResource):
+    def post(self):
+        data = parser.parse_args()
+        current_user = UserModel.find_by_username(data['username'])
+        if not current_user:
+            self.response(
+                {'message': 'User {} doesn\'t exists'.format(data['username'])}, 401
+            )
+            return
+
+        if UserModel.verify_hash(data['password'], current_user.password):
+            access_token = create_access_token(identity=data['username'])
+            refresh_token = create_refresh_token(identity=data['username'])
+            self.response(
+                {
+                    'message': 'Logged in as {}'.format(current_user.username),
+                    'accessToken': access_token,
+                    'refreshToken': refresh_token
+                }, 300
+            )
+        else:
+            self.response({'message': 'Wrong credentials'}, 401)
+
+
+class UserLogoutAccess(BaseResource):
+    @jwt_required
+    def post(self):
+        jti = get_raw_jwt()['jti']
+        try:
+            revoked_token = RevokedTokenModel(jti=jti)
+            revoked_token.add()
+            self.response({'message', 'Access token has been revoked'})
+        except:
+            self.response({'message': 'Something went wrong'}, 500)
+
+
+class UserLogoutRefresh(BaseResource):
+    @jwt_refresh_token_required
+    def post(self):
+        jti = get_raw_jwt()['jti']
+        try:
+            revoked_token = RevokedTokenModel(jti=jti)
+            revoked_token.add()
+            self.response({'message': 'Refresh token has been revoked'})
+        except:
+            self.response({'message': 'Something went wrong'}, 500)
+
+
+class TokenRefresh(BaseResource):
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        access_token = create_access_token(current_user)
+        self.response({'accessToken': access_token})
